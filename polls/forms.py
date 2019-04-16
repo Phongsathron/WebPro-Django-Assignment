fa84ead00072175a15cd506270647249424a9b84
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-04-17 03:41:45 +0700

    add forms validation

--- polls/forms.py
+++ polls/forms.py
@@ -1,8 +1,38 @@
 from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
+
+
+def validate_even(value):
+    if value % 2 != 0:
+        raise ValidationError('%(value)s is not even number', params={'value': value})
 
 
 class PollForm(forms.Form):
     title = forms.CharField(label='ชื่อโพล', max_length=100, required=True)
-    no_question = forms.IntegerField(label='จำนวนคำถาม', min_value=0, max_value=10, required=True)
-    start_date = forms.DateField()
-    end_date = forms.DateField()
+    email = forms.CharField(validators=[validators.validate_email])
+    no_questions = forms.IntegerField(label='จำนวนคำถาม', min_value=0, max_value=10,
+                                      required=True, validators=[validate_even])
+    start_date = forms.DateField(required=False)
+    end_date = forms.DateField(required=False)
+
+    def clean_title(self):
+        data = self.cleaned_data['title']
+
+        if 'ไอทีหมีแพนด้า' not in data:
+            raise forms.ValidationError('คุณลืมชื่อคณะ')
+
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start = cleaned_data.get('start_date')
+        end = cleaned_data.get('end_date')
+
+        if start and not end:
+            # raise forms.ValidationError('โปรดเลือกวันที่สิ้นสุด')
+            self.add_error('end_date', 'โปรดเลือกวันที่สิ้นสุด')
+
+        if end and not start:
+            # raise forms.ValidationError('โปรดเลือกวันที่เริ่มต้น')
+            self.add_error('start_date', 'โปรดเลือกวันที่สิ้นสุด')
